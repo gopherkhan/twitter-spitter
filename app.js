0098bb08b427c9174cddd70917d4b1a87758312e
Chris Nelson <chris@dreamlogic.net>
2018-08-19 00:23:15 -0700

    Refactoring some layout logic. Adding statsy and artsy mode toggles

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 function TweetBlaster(selector) {
 	let content = [];
 	let username =  '';
+	let lastData = [];
 	let preppedSVG = false;
 	const container = document.querySelector(selector);
 	if (!container) { throw `Element ${selector} not found`; }
@@ -17,6 +18,47 @@ function TweetBlaster(selector) {
 	const hoveredWordBlock = statsBlock.querySelector('.word');
 	const hoveredWordCount = statsBlock.querySelector('.count');
 	let wordColors = {};
+	let statsColors = {};
+
+
+	const ARTSY = 1;
+	const STATSY = 2;
+	let mode = ARTSY;
+	var colors = ['#44B3C2', '#F1A94E', '#E45641', '#7B8D8E', 'gold',
+						'#32B92D',	'#FF6EB0',	'#FFCB00',	'#93228D',
+						'#B84B9E', '#F20075'];
+
+	var wordCount = 0;
+	var maxWordLength = 0;
+	var graphWidth = 700;
+	var graphHeight = 500;
+
+	var xScale = d3.scale.linear().range([0, graphWidth]);
+	var yScale = d3.scale.linear().range([0, graphHeight]);
+
+	let numRows = 0; // for statsy mode
+
+	const modeSelector = container.querySelector('.mode-select');
+	modeSelector.addEventListener('click', (e) => {
+		// TODO: Make something better than this
+		if (!e.target.classList.contains('mode')) {
+			return;
+		}
+		modeSelector.querySelector('.mode[selected]').removeAttribute('selected');
+		
+		if (e.target.classList.contains('artsy')) {
+			mode = ARTSY;
+			modeSelector.querySelector('.artsy').setAttribute('selected', '');
+		} else {
+			mode = STATSY;
+			modeSelector.querySelector('.statsy').setAttribute('selected', '');
+		}
+
+		graphData(lastData);
+	});
+
+	modeSelector.innerHTML = `<span class="mode-label">Mode:</span><br><div class="mode statsy">Statsy</div>
+                              <div class="mode artsy" selected>Artsy</div>`;
 
 	form.addEventListener('submit', (e) => {
 		e.preventDefault();
@@ -25,54 +67,153 @@ function TweetBlaster(selector) {
 		fetchData(field.value);
 	});
 
-	function graphData(tweets) {
-		// nonsense for now....
+	function calcXPosition(d, i) {
+		if (mode === ARTSY) {
+			return ~~(((i * 50) / d.occurrences) % graphWidth);
+		}
+		// STATSY
+		return xScale(i % 20 + 1);
+	}
+
+	function calcYPosition(d, i) {
+		if (mode === ARTSY) {
+			return ~~(((d.text.length * 37 )/ d.occurrences) % graphHeight);
+		} 
+		// STATSY
+		return yScale(Math.floor(i / 20) + 1);
+	}
+
+	function calcRadius(d, i) {
+		if (mode === ARTSY) {
+			return 10 * d.text.length;
+		} 
+		return 10;
+	}
+
+	function toggleMode(toSet) {
+		if (toSet === ARTSY || toSet === STATSY) {
+			mode = toSet;
+		} else {
+			mode = ARTSY;
+		}
+	}
+
+	function calcFill(d, i) {
+		if (mode === ARTSY) {
+			return wordColors[d.text];
+		}
+		return statsColors[d.occurrences];
+	}
+
+	function calcOpacity(d, i) {
+		if (mode === ARTSY) {
+			return (d.occurrences / wordCount) + 0.3;
+		}
+		return 0.3 + (d.text.length / maxWordLength) * .6;
+	}
+
+
+	function convertData(tweets) {
 		var rawData = [];
-		var words = {};
-		var wordCount = 0;
-		var stripper = /[^a-z\ ]/gi;
+		var wordHash = {};
+		wordCount = 0; // side effect
+		var maxLength = 0;
+		// var stripper = /[^a-z\ ]/gi;
 
 		tweets.forEach(function(elem) {
-			var stripped = elem.text; // .replace(stripper, "").trim();
+			var stripped = elem.text.trim();
 			var elems = stripped.split(/\W+/gi);
 			wordCount += elems.length;
 			elems.forEach(function(elem) {
-				if (!words[elem]) {
-					words[elem] = 1;
+				if (!wordHash[elem]) {
+					wordHash[elem] = 1;
 				} else {
-					(words[elem])++;
+					(wordHash[elem])++;
 				}
 			});
 			rawData.push({date: elem.date, text: elem.text});
 		});
 
-		let maxCount = 0;
-		Object.keys(words).forEach((w) => {
-			maxCount = Math.max(words[w], maxCount); 
-		});
-
-		var graphWidth = 700;
-		var graphHeight = 500;
-		var colors = ['#44B3C2', '#F1A94E', '#E45641', '#7B8D8E', 'gold',
-						'#32B92D',	'#FF6EB0',	'#FFCB00',	'#93228D',
-						'#B84B9E', '#F20075'];
-		var selection = preppedSVG ? d3.select('#graphy-graph').select('svg') :  d3.select('#graphy-graph').append("svg").attr("width", graphWidth).attr('height',graphHeight);
-
 		var wordsArr = [];
 		wordColors = {};
-		Object.keys(words).forEach(function(key, i) {
+		statsColors = {};
+
+		let maxOccurrences = 0;
+		const uniqueWords = Object.keys(wordHash);
+
+		uniqueWords.forEach(function(key, i) {
 			wordsArr.push({
 				text: key,
-				freq: words[key]
+				occurrences: wordHash[key]
 			});
 			wordColors[key] = colors[i % colors.length];
+			maxOccurrences = Math.max(wordHash[key], maxOccurrences);
+			maxLength = Math.max(maxLength, key.length); 
 		});
 
-		var opacitizer = function(d, i) {
-			return (d.freq / wordCount) + 0.3;
-		};
+		if (mode === STATSY) {
+			wordsArr.sort((a, b) => {
+				if (a.occurrences < b.occurrences) {
+					return -1;
+				}
+				if (a.occurrences > b.occurrences) {
+					return 1;
+				}
+				
+				if (a.text.length < b.text.length) {
+					return -1;
+				}
 
+				if (a.text.length > b.text.length) {
+					return 1;
+				}
+
+				if (a.text < b.text) {
+					return -1;
+				}
+
+				if (a.text > b.text) {
+					return 1;
+				}
+				return 0;
+			});
+
+			for (const word of wordsArr) {
+				if (!statsColors[word.occurrences]) {
+					statsColors[word.occurrences] = true;
+				}
+			}
+			Object.keys(wordColors).forEach((key, i) => {
+				statsColors[key] = colors[i % colors.length];
+			});
+
+			xScale.domain([0, 21]);
+			yScale.domain([0, Math.ceil(wordsArr.length / 20) + 1])
+		} else {
+			xScale.domain([0, maxOccurrences + 1]);
+			yScale.domain([0, maxLength + 1]);
+		}
+		maxWordLength = maxLength;
+		return wordsArr;
+	}
+
+	function getSvg() {
+		if (preppedSVG) {
+			return d3.select('#graphy-graph').select('svg');
+		} 
+		var svg = d3.select('#graphy-graph').append("svg").attr("width", graphWidth).attr('height',graphHeight);
+		preppedSVG = true;
+		return svg;
+	}
+
+
+	function graphData(tweets) {
+		lastData = tweets;
+
+		var selection = getSvg();
+		var wordsArr = convertData(tweets);
 		var circles = selection.selectAll('circle').data(wordsArr);
+
 		circles.enter().append('circle').
 								attr('cx', 0).
 								attr('cy', 0).
@@ -80,7 +221,7 @@ function TweetBlaster(selector) {
 								style('fill', '#999999')
 								.on('mouseover', function(d,i) { 
 									hoveredWordBlock.innerText = d.text;
-									hoveredWordCount.innerText = `${d.freq}/${wordCount}`;
+									hoveredWordCount.innerText = `${d.occurrences}/${wordCount}`;
 								});
 
 		var removeIdx = 0;
@@ -104,21 +245,13 @@ function TweetBlaster(selector) {
 				attr('data-name', function(d, i) {
 					return d.text;
 				}).attr('data-count', function(d, i) {
-					return d.freq;
+					return d.occurrences;
 				})
-				.attr('cx', function(d, i) {
-					return ~~(((i * 50) / d.freq) % graphWidth);
-					// return ~~((d.freq / maxCount) * graphWidth + (d.freq / wordCount) * 50) % graphWidth;
-				}).attr('cy', function(d, i) {
-					return ~~(((d.text.length * 37 )/ d.freq) % graphHeight);
-				}).attr('r', function(d, i) {
-					return 10 * d.text.length;
-				}).style('fill', function(d, i) {
-					//d.text.length
-					return wordColors[d.text];
-				}).style('opacity', opacitizer);
-
-		preppedSVG = true;
+				.attr('cx', calcXPosition)
+				.attr('cy', calcYPosition)
+				.attr('r', calcRadius)
+				.style('fill', calcFill)
+				.style('opacity', calcOpacity);
 	}
 
 
@@ -161,8 +294,8 @@ function TweetBlaster(selector) {
 		const coloredWords = dat.text.split(/\W+/gi).map((word) => {
 			const color = wordColors[word] || '#333';
 		 	return `<span style="color:${color};">${word}</span>`; 
-		}).join(' ');
-		*/
+		}).join(' '); */
+		
 		return `<li>
 					<img src="${dat.avatar}" class="person-image" >
 					<span class="date">${dat.date}</span>
